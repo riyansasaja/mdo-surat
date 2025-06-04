f6f23bd656dd9d9d5eed1c80aa30162f0a0809b8
riyansasaja <riyanboyuhu@gmail.com>
2025-06-04 12:05:10 +0800

    menambahkan tamplate sederhana untuk tampilan surat keluar

--- app/Views/outmail/show.php
+++ app/Views/outmail/show.php
@@ -1,26 +1,36 @@
 <?= $this->extend('home/layouts') ?>
 <?= $this->section('main') ?>
 
-<?php
-//dd($inmaildespo);
-?>
 
-    <div class="container-fluid px-4">
-        <img class="img-fluid" src="<?= base_url('assets/img/background-under-construction-download.png') ?>" alt="bg_underConstruction">
+<div class="row ms-3 mt-4">
+
+    <div class="col-md-3 ">
+        di sini torang mo bekeng depe card 3 x 3
     </div>
+    <div class="col-md-3">
+        di sini torang mo bekeng depe card 3 x 3
+    </div>
+    <div class="col-md-3">
+        di sini torang mo bekeng depe card 3 x 3
+    </div>
+    <div class="col-md-3">
+        di sini torang mo bekeng depe card 3 x 3
+    </div>
+
+</div>
 
 
 
 <?= $this->endSection() ?>
 
 <?= $this->section('pageScripts') ?>
-    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
-    <script src="<?= base_url() ?>js/inmail.js"></script>
-    <script>
-        let message = <?= json_encode(session()->getFlashdata('message')) ?>;
-        let success = <?= json_encode(session()->getFlashdata('success')) ?>;
-        let error = <?= json_encode(session()->getFlashdata('error')) ?>;
-        let path = <?= json_encode(base_url()) ?>;
-    </script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+<script src="<?= base_url() ?>js/inmail.js"></script>
+<script>
+    let message = <?= json_encode(session()->getFlashdata('message')) ?>;
+    let success = <?= json_encode(session()->getFlashdata('success')) ?>;
+    let error = <?= json_encode(session()->getFlashdata('error')) ?>;
+    let path = <?= json_encode(base_url()) ?>;
+</script>
 
 <?= $this->endSection() ?>
