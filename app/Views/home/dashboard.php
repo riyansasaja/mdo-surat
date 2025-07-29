eea3058dcbadbd9a5be29145252a71fd252a5b5c
riyansasaja <riyanboyuhu@gmail.com>
2025-07-29 14:26:25 +0800

    menghilangkan total surat keluar

--- app/Views/home/dashboard.php
+++ app/Views/home/dashboard.php
@@ -28,7 +28,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-xl-3 col-md-6">
+        <!-- <div class="col-xl-3 col-md-6">
             <div class="card bg-warning text-white mb-4">
                 <div class="card-body">Total Surat Keluar</div>
                 <div class="card-footer d-flex align-items-center justify-content-between">
@@ -36,7 +36,7 @@
                     <div class="small text-white"><i class="fas fa-angle-right"></i></div>
                 </div>
             </div>
-        </div>
+        </div> -->
         <div class="col-xl-3 col-md-6">
             <div class="card bg-success text-white mb-4">
                 <div class="card-body">Total Surat Masuk ditindaklanjuti</div>
